905a5fca4a4b8fb0343c888797525a0ad3247617
tousif644 <Tousif2567@gmail.com>
2021-12-27 22:36:41 +0600

    All Done

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+
+
+sr.reveal('.container', {distance: '20px', delay: 120, interval: 100})
+sr.reveal('.btn', {delay: 400})
+sr.reveal('.animation-container' , {distance:"40px" , delay:300 , interval:50})
+sr.reveal('.home-text' , {distance:"50px" , delay:500, interval:60})
+sr.reveal('.navbar' , {distance:"60px" , delay:700 , interval:70})
+sr.reveal('.hire-me', {distance:"70px" , delay:900 , interval:80})
+sr.reveal('.love-button', {distance:"70px" , delay:900 , interval:80})
+sr.reveal('.skill-image' , {distance:"90px" , delay:1000 , interval:90})
+
+sr.reveal('.progress',{distance:'50px' ,delay:50, interval:100});
+sr.reveal('.skill-text',{distance:'50px' ,delay:50, interval:100});
